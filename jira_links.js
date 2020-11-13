300ac69d7fac4f58a5080526d0d2b6248ef5dfbd
Nem <nemnesic@gmail.com>
2020-11-12 22:36:26 -0500

    version 2 fix

--- jira_links.js
+++ jira_links.js
@@ -1,3 +1,6 @@
-jQuery(document).ready(function() {
-  jQuery(".external-link").attr("target", "_blank");
+jQuery(document).ready(function () {
+    jQuery('a[data-renderer-mark="true"]').each(function (i, obj) {
+        var link = obj.innerText;
+        jQuery(this).append(' | <a class="my-opener" style="color:#FF0000;" target="_blank" href="' + link + '">Open in new tab</a>');
+    });
 });
